19f0466a455ccfabf86ec2b5e36f6f6563b1700d
Ponk8 <lucas.alencar31@gmail.com>
2022-12-03 18:34:32 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let currentNumber = document.querySelector('label#current-number')
+let n0 = document.querySelector('div#zero')
+let n1 = document.querySelector('div#one')
+let n2 = document.querySelector('div#two')
+let n3 = document.querySelector('div#three')
+let n4 = document.querySelector('div#four')
+let n5 = document.querySelector('div#five')
+let n6 = document.querySelector('div#six')
+let n7 = document.querySelector('div#seven')
+let n8 = document.querySelector('div#eight')
+let n9 = document.querySelector('div#nine')
+
+
+
+
+
+
+let bntEquals = document.querySelector('button#equals')
+.addEventListener('click', () => {
+    return console.log('uiuiui')
+})
