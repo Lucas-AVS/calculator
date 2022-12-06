2b91e3ae222ced3e5ea53d1acaf35f7a5e250baa
Ponk8 <lucas.alencar31@gmail.com>
2022-12-05 23:52:40 -0300

    feat: add and subtract function + getNumber

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
-let currentNumber = document.querySelector('label#current-number')
-let n0 = document.querySelector('div#zero')
-let n1 = document.querySelector('div#one')
-let n2 = document.querySelector('div#two')
-let n3 = document.querySelector('div#three')
-let n4 = document.querySelector('div#four')
-let n5 = document.querySelector('div#five')
-let n6 = document.querySelector('div#six')
-let n7 = document.querySelector('div#seven')
-let n8 = document.querySelector('div#eight')
-let n9 = document.querySelector('div#nine')
+let displayNumber = document.querySelector('label#current-number')
+let currentNumber = '';
+let arrayNumber = [];
+
+let loop = 0
+
+let numbers = document.querySelector('div.numbers')
+
+let add = document.querySelector('button#add').addEventListener('click', () => {
+    arrayNumber.push(Number(currentNumber));
+    currentNumber = '';
+    displayNumber.innerHTML += '+';
+    console.log(currentNumber)
+})
+
+let subtract = document.querySelector('button#subtract').addEventListener('click', () => {
+    arrayNumber.push(Number(currentNumber));
+    currentNumber = '-';
+    displayNumber.innerHTML += '-';
+    console.log(currentNumber)
+})
 
 
+function getNumber(n) {
+    for(let i = n; i >= 0; i--) {
+        let button = document.createElement('button');
+        button.setAttribute("id", i);
+        numbers.appendChild(button);
+        button.innerText = i
+        button.addEventListener('click', () => {
+            if (!displayNumber.innerHTML.includes('+', '-', '÷', 'x') && loop >0 ) {
+                displayNumber.innerHTML = i;
+                arrayNumber = [i];
+                loop = 0;
+            }
+            else { 
+                currentNumber += i
+                displayNumber.innerHTML += i;
+            }
+        })
+    }
+}
+
+getNumber(9)
 
 
 
 
 let bntEquals = document.querySelector('button#equals')
 .addEventListener('click', () => {
-    return console.log('uiuiui')
+    console.log(currentNumber)
+    arrayNumber.push(Number(currentNumber));
+    currentNumber = '';
+    displayNumber.innerHTML = arrayNumber.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+    loop++
 })
